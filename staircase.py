114fd33ae06d296cafdc5f772e5ae1666c9448c8
lugrao <elugrao@gmail.com>
2021-06-09 21:36:37 -0300

    staircase

---
+++ staircase.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the staircase function below.
+def staircase(n, hashes=1):
+    print(" " * (n - hashes) + "#" * hashes)
+
+    if hashes == n:
+        return
+    return staircase(n, hashes + 1)
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    staircase(n)
